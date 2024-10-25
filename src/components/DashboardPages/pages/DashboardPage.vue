0e9a260f753175b5635808c7f0d649bdd412e4c5
thethermopylae <amirmahdi.brzgr1386@gmail.com>
2024-10-25 15:41:59 +0330

    dashboard pages tranisiton

--- src/components/DashboardPages/pages/DashboardPage.vue
+++ src/components/DashboardPages/pages/DashboardPage.vue
@@ -1,20 +1,25 @@
 <template>
     <div class=" relative">
         <SidebarDashboard></SidebarDashboard>
-        <router-view class="absolute left-0 w-4/5 mr-10 p-5 h-screen overflow-y-auto overflow-x-hidden"></router-view>
+        <router-view v-slot="{ Component }"
+            class="absolute left-0 w-4/5 mr-10 p-5 h-screen overflow-y-auto overflow-x-hidden">
+            <transition>
+                <component :is="Component" />
+            </transition>
+        </router-view>
     </div>
 </template>
 
 <script>
 import SidebarDashboard from '../SidebarDashboard.vue';
-export default{
-    components:{
+export default {
+    components: {
         SidebarDashboard
     },
-    setup(){
+    setup() {
 
-        return{
-            
+        return {
+
         }
     }
 }
