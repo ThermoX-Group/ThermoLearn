0586e02b3599643ec10be1467ddbe7a72bf30990
thethermopylae <amirmahdi.brzgr1386@gmail.com>
2024-10-27 23:51:31 +0330

    Redesign ModalDadaBox

--- src/components/DashboardPages/EditDataModal.vue
+++ src/components/DashboardPages/EditDataModal.vue
@@ -2,7 +2,7 @@
     <div class="fixed top-0 right-0 w-full h-full z-20">
         <div class="blur w-full h-full" @click="$emit('closeModal')"></div>
         <div
-            class="bg-yellow-100 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 py-3 px-5 rounded-xl w-2/3 h-3/4 overflow-scroll lg:w-1/2 lg:h-auto lg:overflow-visible">
+            class="bg-base-300 absolute top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 py-3 px-5 rounded-xl w-2/3 h-3/4 overflow-scroll lg:w-1/2 lg:h-auto lg:overflow-visible">
             <div class="flex justify-between items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="size-6 cursor-pointer" @click="$emit('closeModal')">
@@ -14,46 +14,46 @@
                 <div class="flex  flex-col justify-between">
                     <div>
                         <label for="name">نام دوره</label>
-                        <input type="text" id="name" class="w-full border-2 rounded-full p-2 outline-yellow-300 mt-2"
+                        <input type="text" id="name" class="w-full border-2 rounded-full p-2 outline-yellow-300 bg-base-100 mt-2"
                             placeholder="نام قسمت دوره را وارد کنید..." v-model="courseData.courseName">
                     </div>
                     <div>
                         <label for="reprerequisite">پیش نیاز دوره</label>
                         <input type="text" id="reprerequisite"
-                            class="w-full border-2 rounded-full p-2 outline-yellow-300 mt-2"
+                            class="w-full border-2 rounded-full p-2 outline-yellow-300 bg-base-100 mt-2"
                             placeholder="پیش نیاز دوره را وارد کنید..." v-model="courseData.courseReprerequisite">
                     </div>
                     <div>
                         <label for="cover">کاور دوره</label>
-                        <input type="text" id="cover" class="w-full border-2 rounded-full p-2 outline-yellow-300 mt-2"
+                        <input type="text" id="cover" class="w-full border-2 rounded-full p-2 outline-yellow-300 bg-base-100 mt-2"
                             placeholder="کاور دوره را وارد کنید..." v-model="courseData.img">
                     </div>
                 </div>
                 <div>
                     <label for="des">توضیحات دوره</label>
                     <textarea id="des" placeholder="توضیحات دوره را وارد کنید..."
-                        class="block w-full border-2 rounded-xl p-2 outline-yellow-300 mt-2"
+                        class="block w-full border-2 rounded-xl p-2 outline-yellow-300 bg-base-100 mt-2"
                         v-model="courseData.courseDes" rows="8"></textarea>
                 </div>
                 <div>
                     <label for="price">قیمت دوره</label>
-                    <input type="number" id="price" class="w-full border-2 rounded-full p-2 outline-yellow-300 mt-2"
+                    <input type="number" id="price" class="w-full border-2 rounded-full p-2 outline-yellow-300 bg-base-100 mt-2"
                         placeholder="قیمت دوره را وارد کنید..." v-model="courseData.coursePrice">
                 </div>
                 <div>
                     <label for="time">مدت زمان تقریبی دوره</label>
-                    <input type="number" id="time" class="w-full border-2 rounded-full p-2 outline-yellow-300 mt-2"
+                    <input type="number" id="time" class="w-full border-2 rounded-full p-2 outline-yellow-300 bg-base-100 mt-2"
                         placeholder="مدرس دوره را وارد کنید..." v-model="courseData.courseTime">
                 </div>
             </div>
             <div>
                 <label for="teacher" class="mt-3 block">مدرس دوره</label>
                 <input type="text" id="teacher"
-                    class="w-full bg-white border-2 rounded-full p-2 outline-yellow-300 mt-2"
+                    class="w-full border-2 rounded-full p-2 outline-yellow-300 bg-base-100 mt-2"
                     placeholder="مدرس دوره را وارد کنید..." v-model="courseData.courseTeach">
             </div>
             <button @click="updateReqFunc"
-                class="w-full rounded-full transition bg-yellow-300 border-2 border-yellow-300 hover:bg-yellow-100 hover:border-yellow-300 mt-4 p-2">
+                class="w-full text-black rounded-full transition bg-yellow-300 border-2 border-yellow-300 hover:bg-yellow-100 hover:border-yellow-300 mt-4 p-2">
                 <slot></slot>
             </button>
         </div>
